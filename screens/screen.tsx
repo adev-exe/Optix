733b830499bc63909d1fcfa89d1db82deb722625
Abelson Abueg <abuega18@students.ecu.edu>
2020-10-17 22:18:59 -0400

    Updates

--- screens/screen.tsx
+++ screens/screen.tsx
@@ -1,45 +1,84 @@
-import React, {Component} from 'react';
+import React, {Component, useState, useEffect } from 'react';
 import { Image, StyleSheet, Text, TouchableHighlight, View} from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { StatusBar } from 'expo-status-bar';
-
 interface Screen {
 
 }
 
 const styles = StyleSheet.create({
     wrapper: {
+        backgroundColor: '#555',
         height: '100%',
         width: '100%',
+        display: 'flex',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
+    scanButton: {
+        width: '50%',
+        height: '10%',
+        margin: '10%',
+        backgroundColor: '#0066ff',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 360,
+    },
+    replayButton: {
+        width: '50%',
+        height: '10%',
+        margin: '10%',
+        backgroundColor: '#ff9900',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 360,
+    },
+    buttonText: {
+        textAlign: 'center',
+        color: '#FFF',
+        fontWeight: 'bold',
+        fontSize: 60,
+    }
 });
 
 export class Screen extends Component <{}, Screen> {
-    constructor(props : any) {
-        super(props);
-        this.state = {
+    async ComponentDidMount() {
+        const { status } = await Camera.requestPermissionsAsync();
 
+        if (status !== "granted") {
+            throw new Error("No access to camera");
         }
+        else {
+            console.log(status);
+        }
+    }
+
+    async componentWillUnmount() {
     }
 
     render() {
         return(
-            <SafeAreaProvider>
-                <StatusBar />
-
-                    <TouchableHighlight>
-                        <View>
-                            <Text>Scan</Text>
-                        </View>
-                    </TouchableHighlight>
-                    <TouchableHighlight>
-                        <View>
-                            <Text>Replay Audio</Text>
-                        </View>
-                    </TouchableHighlight>
-
-             </SafeAreaProvider>
-
+            <View style={styles.wrapper}>
+                <TouchableHighlight
+                    style={styles.scanButton}
+                    //onPress={}
+                >
+                    <View>
+                        <Text style={styles.buttonText}>
+                            Scan
+                        </Text>
+                    </View>
+                </TouchableHighlight>
+                <TouchableHighlight
+                    style={styles.replayButton}
+                    //onPress={}
+                >
+                    <View>
+                        <Text style={styles.buttonText}>
+                            Replay
+                        </Text>
+                    </View>
+                </TouchableHighlight>
+            </View>
         );
     }
 }
